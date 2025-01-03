7558acb2e6e676125c38213ab466a62940b9e1f2
Anwesha-Bhattacharjee <mohor122@gmail.com>
2025-01-03 23:07:48 +0530

    parameterized browsers

--- src/test/java/testcases/TestBase.java
+++ src/test/java/testcases/TestBase.java
@@ -4,8 +4,12 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import utilities.ConfigReader;
 
@@ -15,10 +19,26 @@ public WebDriver driver;
 Properties properties;
 
 @BeforeClass
-public void setup() {
+@Parameters("browser")
+public void setup(String browser) {
 	ConfigReader configreader = new ConfigReader();
 	properties = configreader.getProperties();
-	driver = new ChromeDriver();
+	//driver = new ChromeDriver();
+	DesiredCapabilities capabilities = new DesiredCapabilities();
+	switch(browser.toLowerCase()) {
+	case"chrome":
+		driver = new ChromeDriver();
+		break;
+	case "firefox": 
+		driver = new FirefoxDriver();
+		break;
+	case "edge":
+		driver = new EdgeDriver();
+		break;
+	default: 
+		System.out.println("Invalid browser provided: " + browser);
+		return; 
+	}	
 }
 
 @AfterClass
