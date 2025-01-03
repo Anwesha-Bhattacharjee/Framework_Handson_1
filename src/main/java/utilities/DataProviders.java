204cceddeec917b967dd9325a9ad745c5f364946
Anwesha-Bhattacharjee <mohor122@gmail.com>
2025-01-04 00:02:09 +0530

    added cross browser parallel suite xml, fixed DDT bugs

--- src/main/java/utilities/DataProviders.java
+++ src/main/java/utilities/DataProviders.java
@@ -8,12 +8,12 @@ public class DataProviders {
 	@DataProvider(name = "dp1")
 	public Object[][] dataProvider1() throws IOException{
 		String path = "src\\test\\resources\\testdata\\data.xlsx";
-		String sheetName = "Sheet1";
+		String sheetName = "Sheet2";
 		ExcelUtility eu= new ExcelUtility(path,sheetName);
 		int rowCount = eu.getRowCount();		
 		int colCount = eu.getColCount();
-		
-		Object[][] data = new Object[rowCount][colCount];
+		//System.out.println(rowCount+" "+colCount);
+		Object[][] data = new Object[rowCount-1][colCount];
 		
 		for(int i= 1; i<rowCount; i++) {
 			for(int j= 0; j<colCount; j++) {
--- src/test/java/testcases/FetchWebsiteTitleDDT.java
+++ src/test/java/testcases/FetchWebsiteTitleDDT.java
@@ -1,13 +1,16 @@
 package testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import utilities.DataProviders;
 
-public class FetchWebsiteTitleDDT {
+public class FetchWebsiteTitleDDT extends TestBase {
 
 	@Test(dataProvider = "dp1", dataProviderClass = DataProviders.class)
 	public void fetchWebsiteDetailsAndMatch(String id, String url, String title) {
+		driver.navigate().to(url);
 		System.out.println(id + url + title);
+		Assert.assertEquals(driver.getTitle(), title);
 	}
 }
